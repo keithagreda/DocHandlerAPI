8f8330f5d2eafc8a834d6013170d4d7d3296cab4
Keith Agreda <104190595+keithagreda@users.noreply.github.com>
2025-12-01 00:56:47 +0800

    moved PDF generator Interface into Interfaces folder

---
+++ DocumentHandlerAPI/Interfaces/IPdfGeneratorService.cs
@@ -0,0 +1,9 @@
+﻿using DocumentHandlerAPI.Models.Dtos;
+
+namespace DocumentHandlerAPI.Interfaces
+{
+    public interface IPdfGeneratorService
+    {
+        Task<ApiResponse<string>> GeneratePDFAsync(PdfGenerationRequest req);
+    }
+}
