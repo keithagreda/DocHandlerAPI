dbeb8db5018d7218c978d21712beed304d86ffd8
Keith Agreda <104190595+keithagreda@users.noreply.github.com>
2025-12-01 07:43:41 +0800

    removed default api endpoint

--- DocumentHandlerAPI/Program.cs
+++ DocumentHandlerAPI/Program.cs
@@ -76,25 +76,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapPost("/api/documents/generate-pdf", async (
     [FromBody] PdfGenerationRequest request,
     [FromServices] IPdfGeneratorService pdfService) =>
@@ -108,8 +89,3 @@ app.MapPost("/api/documents/generate-pdf", async (
 .WithName("GeneratePDF");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
